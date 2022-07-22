ceac5817f75b4e80d123ee5a560b041c8f5796ec
Ghostipedia <Ghostipedia@gmail.com>
2022-07-22 05:51:58 -0400

    Initial commit

---
+++ src/main/java/net/ghostipedia/simplyforgotten/SimplyForgottenClient.java
@@ -0,0 +1,10 @@
+package net.ghostipedia.simplyforgotten;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SimplyForgottenClient implements ClientModInitializer {
+        @Override
+        public void onInitializeClient() {
+
+        }
+}
